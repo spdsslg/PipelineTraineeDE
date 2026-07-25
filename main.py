78a562411b2691ff1b612db822a768e30dd774ef
spdsslg <mmoroz5656@gmail.com>
2026-07-25 14:41:12 +0200

    refactored DBManager to be a singleton

--- main.py
+++ main.py
@@ -2,71 +2,85 @@ import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import connection
 
-db_name = "dormitory"
+def singleton(class_):
+    instances = {}
 
-def create_db():
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+@singleton
+class DBManager():
     """
-    Create a database `db_name` if doesn't exist
+    DBManager class encapsulates the connection and creation of the schema
     """
-    postgres_conn = psycopg2.connect(dbname='postgres', user='postgres', host='localhost', password='postgres')
+    #class attributes of decorated class can't be accessed through the class name!
+    db_name = "dormitory" 
 
-    postgres_conn.autocommit = True
+    def __init__(self):
+        self._create_db()
 
-    with postgres_conn.cursor() as cur:
-        cur.execute("SELECT 1 FROM pg_database WHERE datname=%s;", (db_name,))
-        exists = cur.fetchone()
+        self.conn = psycopg2.connect(user='postgres', password='postgres', dbname=self.db_name, host='localhost')
 
-        if not exists:
-            #avoids injection
-            cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name)))
-            print(f"Created {db_name}")
-        else:
-            print(f"{db_name} already exists")
-    
-    postgres_conn.close()
+        self._create_tables()
 
-def create_tables(conn: connection):
-    student_create = """ 
-    CREATE TABLE IF NOT EXISTS student (
-        id int NOT NULL,
-        room int NOT NULL,
-        name varchar(50) NOT NULL,
-        birthday DATE NOT NULL,
-        sex VARCHAR(1) NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_room_student 
-            FOREIGN KEY (room)
-            REFERENCES room(id)
-    );
-    """
+    def _create_db(self):
+        """
+        Create a database `db_name` if doesn't exist
+        """
+        postgres_conn = psycopg2.connect(dbname='postgres', user='postgres', host='localhost', password='postgres')
 
-    room_create = """
-    CREATE TABLE IF NOT EXISTS room (
-        id int NOT NULL,
-        name varchar(25) NOT NULL,
-        PRIMARY KEY (id)
-    );
-    """
+        postgres_conn.autocommit = True
 
-    student_add_constraints = """
-    ALTER TABLE student
-    ADD CONSTRAINT fk_room_student FOREIGN KEY (room)
-    REFERENCES room(id);
-    """
+        with postgres_conn.cursor() as cur:
+            cur.execute("SELECT 1 FROM pg_database WHERE datname=%s;", (self.db_name,))
+            exists = cur.fetchone()
 
-    #automatically commits or rolls back
-    with conn: 
-        with conn.cursor() as cur:
-            cur.execute(room_create)
-            cur.execute(student_create)
-            print("Tables 'student' and 'room' were created/already exist")
+            if not exists:
+                #avoids injection
+                cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(self.db_name)))
+                print(f"Created {self.db_name}")
+            else:
+                print(f"{self.db_name} already exists")
+        
+        postgres_conn.close()
 
+    def _create_tables(self):
+        student_create = """ 
+        CREATE TABLE IF NOT EXISTS student (
+            id int NOT NULL,
+            room int NOT NULL,
+            name varchar(50) NOT NULL,
+            birthday DATE NOT NULL,
+            sex VARCHAR(1) NOT NULL,
+            PRIMARY KEY (id),
+            CONSTRAINT fk_room_student 
+                FOREIGN KEY (room)
+                REFERENCES room(id)
+        );
+        """
+
+        room_create = """
+        CREATE TABLE IF NOT EXISTS room (
+            id int NOT NULL,
+            name varchar(25) NOT NULL,
+            PRIMARY KEY (id)
+        );
+        """
+
+        #automatically commits or rolls back
+        with self.conn: 
+            with self.conn.cursor() as cur:
+                cur.execute(room_create)
+                cur.execute(student_create)
+                print("Tables 'student' and 'room' were created/already exist")
+
+    # def close_connection(self):
+    #     self.conn.close()
 
 if __name__ == '__main__':
-    create_db()
+    db_manager = DBManager()
 
-    conn = psycopg2.connect(user='postgres', password='postgres', dbname=db_name, host='localhost')
-
-    create_tables(conn)
-
-    conn.close()
+    
